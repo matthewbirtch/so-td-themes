753905dbb8556c1b882056750ce61fd66ae497a6
Matthew Birtch <matthew.birtch@mosaic.com>
2014-04-30 14:42:01 -0400

    fixed posts

--- tres-dias-one/index.php
+++ tres-dias-one/index.php
@@ -56,17 +56,18 @@
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<div class="page-container-outer">
 				<div class="page-container-inner">
-				<div class="content-container blog">
-					<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-						<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-						<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-						<div class="entry">
-							<?php the_content(); ?>
-						</div>
-						<div class="postmetadata">
-							<?php the_tags('Tags: ', ', ', '<br />'); ?>
-							Posted in <?php the_category(', ') ?> | 
-							<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+					<div class="content-container blog">
+						<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+							<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+							<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+							<div class="entry">
+								<?php the_content(); ?>
+							</div>
+							<div class="postmetadata">
+								<?php the_tags('Tags: ', ', ', '<br />'); ?>
+								Posted in <?php the_category(', ') ?> | 
+								<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+							</div>
 						</div>
 					</div>
 				</div>
